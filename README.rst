2feb1060545bf3f085b4fd58e74a68537f293a61
Lakshmi Vyasarajan <lakshmi.vyas@gmail.com>
2012-03-08 09:48:29 +0530

    Extracted from hyde

---
+++ README.rst
@@ -0,0 +1,11 @@
+Version 0.1.0
+
+Unified object oriented interface for interacting with file system objects.
+File system operations in python are distributed across modules: os, os.path,
+fnamtch, shutil and distutils. This module attempts to make the right choices
+for common operations to provide a single interface.
+
+This was extracted from the `hyde project`_ with minor changes.
+
+
+.. _hyde project: http://github.com/hyde/hyde
---
+++ setup.py
@@ -0,0 +1,32 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='fswrap',
+    version='0.1.0',
+    author='Lakshmi Vyas',
+    author_email='lakshmi.vyas@gmail.com',
+    url='http://github.com/lakshmivyas/fswrap',
+    description='An opinionated wrapper on file system and path functions',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    py_modules=['fswrap'],
+    tests_require=(
+        'nose',
+      ),
+    test_suite='nose.collector',
+)
